e03cf766e8b41781ac2235b0d2f17f6bdef1e8cc
ilongge <1015820857@qq.com>
2022-03-15 14:07:33 +0800

    init repo

---
+++ Example/WNPlayer-ilongge/WNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WNAppDelegate.h
+//  WNPlayer-ilongge
+//
+//  Created by 1015820857@qq.com on 03/15/2022.
+//  Copyright (c) 2022 1015820857@qq.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface WNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

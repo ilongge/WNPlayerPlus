32866bc75fe6baf9cb396366bbc65609baf0bda6
ilongge <1015820857@qq.com>
2022-03-15 14:08:25 +0800

    add FFMpeg.Frameworks

---
+++ Frameworks/FFmpeg.framework/Headers/ffmpeg.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+#import <VideoToolbox/VideoToolbox.h>
+#import <AudioToolbox/AudioToolbox.h>
+#include <FFmpeg/avcodec.h>
+#include <FFmpeg/avdevice.h>
+#include <FFmpeg/avfilter.h>
+#include <FFmpeg/avformat.h>
+#include <FFmpeg/avutil.h>
+#include <FFmpeg/swscale.h>
+#include <FFmpeg/swresample.h>
+#include <FFmpeg/imgutils.h>

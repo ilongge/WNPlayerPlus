932f8958d9d0ec46c299960f7dd19019ad534cd7
ilongge <1015820857@qq.com>
2022-04-13 10:32:22 +0800

    add file

--- package.sh
+++ package.sh
@@ -1,3 +1,6 @@
+
+Spec=WNPlayerPlus.podspec
+
 current_path=$(
     cd "$(dirname "$0")"
     pwd
@@ -7,7 +10,7 @@ echo into $current_path
 
 cd $current_path
 
-echo "pod spec lint WNPlayer-ilongge.podspec --allow-warnings"
+echo "pod spec lint $Spec --allow-warnings"
 validate=$(pod spec lint WNPlayer-ilongge.podspec --allow-warnings)
 
 Not_Pass_Word="The spec did not pass validation"
@@ -21,15 +24,15 @@ fi
 
 echo "The spec did pass validation"
 
-echo "pod trunk push --allow-warnings WNPlayer-ilongge.podspec"
-Push_Result=$(pod trunk push --allow-warnings WNPlayer-ilongge.podspec)
-
-echo "pod package WNPlayer-ilongge.podspec --force --no-mangle"
-Package_Result=$(pod package WNPlayer-ilongge.podspec --force --no-mangle)
-Configuration_Release= "with configuration Release"
-result=$(echo $Package_Result | grep "${Configuration_Release}")
-if [[ "$result" != "" ]]; then
-    echo "Framework Build Fail"
-else
-    echo "Framework Build Success"
-fi
+#echo "pod trunk push --allow-warnings $Spec"
+#Push_Result=$(pod trunk push --allow-warnings WNPlayer-ilongge.podspec)
+#
+#echo "pod package $Spec --force --no-mangle"
+#Package_Result=$(pod package WNPlayer-ilongge.podspec --force --no-mangle)
+#Configuration_Release= "with configuration Release"
+#result=$(echo $Package_Result | grep "${Configuration_Release}")
+#if [[ "$result" != "" ]]; then
+#    echo "Framework Build Fail"
+#else
+#    echo "Framework Build Success"
+#fi
